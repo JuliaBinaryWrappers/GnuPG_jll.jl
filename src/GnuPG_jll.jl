7bf6a798ab0e88c4b66f0a9c8aeb27f9d9702cae
jlbuild <juliabuildbot@gmail.com>
2026-01-02 01:52:27 +0000

    GnuPG_jll build 2.5.16+0

--- src/GnuPG_jll.jl
+++ src/GnuPG_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("GnuPG")
-JLLWrappers.@generate_main_file("GnuPG", UUID("1522389b-45f8-5faa-af4d-a301b79c50ac"))
+JLLWrappers.@generate_main_file("GnuPG", Base.UUID("1522389b-45f8-5faa-af4d-a301b79c50ac"))
 end  # module GnuPG_jll
